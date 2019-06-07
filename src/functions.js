ac471418c774a07343d915c325fb258726607c3f
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-06-07 15:56:38 -0700

    Output data for one doctor

--- src/functions.js
+++ src/functions.js
@@ -17,7 +17,7 @@ export class DoctorFinder {
 
   connectToApi(options) {
   const API_KEY = process.env.exports.apiKey;
-  let promise = new Promise(function(resolve, reject) {
+  return new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
     let url = `https://api.betterdoctor.com/2016-03-01/${options}location=or-portland&user_location=45.5206223%2C-122.6773984&skip=0&limit=10&user_key=${API_KEY}`;
     request.onload = function() {
--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { DoctorFinder } from './functions';
 
+function getDoctorInfo(body, option) {
+  $('#result').text(`The doctors with the name ${option} include... First Name: ${body.data[0].profile.first_name} Last name: ${body.data[0].profile.last_name} Address: ${body.data[0].practices[0].visit_address["street"]}. ${body.data[0].practices[0].visit_address["city"]}, ${body.data[0].practices[0].visit_address["state"]} Phone Number: ${body.data[0].practices[0].phones[0].number} Accepting New Patients: ${body.data[0].practices[0].accepts_new_patients} <a href="${body.data[0].profile["image_url"]}"> Pic Of Me</a> `);
+}
+
 $(document).ready(function() {
   let find = new DoctorFinder();
   $("#nameForm").submit(function(event) {
@@ -12,10 +16,13 @@ $(document).ready(function() {
     let option = $("#name").val();
     let optionType = $("#optionType1").val();
     let options = find.addToOptions(option, optionType);
-    find.connectToApi(options);
-
+    
+    let promise = find.connectToApi(options);
+    
     promise.then(function(response) {
-      $('#result').text(`The doctors with the name ${option} include ${body.data.doctors[0].first_name}%`);
+      let body = JSON.parse(response);
+      console.log(body);
+      getDoctorInfo(body, option);
     }, function(error) {
       $("#result").text(`Sorry, no doctors have the name ${option} in your area.`);
     });
@@ -25,11 +32,16 @@ $(document).ready(function() {
     event.preventDefault();
     let option = $("#practice").val();
     let optionType = $("#optionType2").val();
+    let options = find.addToOptions(option, optionType);
+
+    let promise = find.connectToApi(options);
 
     promise.then(function(response) {
+      let body = JSON.parse(response);
       $('#result').text(`The doctors that practice ${option} are ${body.data.doctors[0].first_name}`);
     }, function(error) {
-      $("#result").text(`Sorry, no doctors practice ${option} in your area.`);
+      // $("#result").text(`Sorry, no doctors practice ${option} in your area.`);
+      $("#errorMessage").text(`There was an error processing your request: ${error.message}`);
     });
   });
 });
