8d2ad3ff76c24cd69f7483ba998aa835330a6e02
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-06-07 16:31:38 -0700

    Edit getDoctorInfo()

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-// require('exports-loader?file!./bootstrap/js/dist/.js')
 import $ from 'jquery';
-// import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { DoctorFinder } from './functions';
 
 function getDoctorInfo(body, option) {
-  $('#result').text(`The doctors with the name ${option} include... First Name: ${body.data[0].profile.first_name} Last name: ${body.data[0].profile.last_name} Address: ${body.data[0].practices[0].visit_address["street"]}. ${body.data[0].practices[0].visit_address["city"]}, ${body.data[0].practices[0].visit_address["state"]} Phone Number: ${body.data[0].practices[0].phones[0].number} Accepting New Patients: ${body.data[0].practices[0].accepts_new_patients} <a href="${body.data[0].profile["image_url"]}"> Pic Of Me</a> `);
+  // not sure why loop currently wont work
+  body.data.forEach(function(item) {
+    $('#result').text(`The doctors with the name ${option} include... First Name: ${item.profile.first_name} Last name: ${item.profile.last_name} Address: ${item.practices[0].visit_address["street"]}. ${item.practices[0].visit_address["city"]}, ${item.practices[0].visit_address["state"]} Phone Number: ${item.practices[0].phones[0].number} Accepting New Patients: ${item.practices[0].accepts_new_patients} <a href="${item.profile["image_url"]}"> Pic Of Me</a> `);
+  });
 }
 
 $(document).ready(function() {
@@ -16,15 +17,16 @@ $(document).ready(function() {
     let option = $("#name").val();
     let optionType = $("#optionType1").val();
     let options = find.addToOptions(option, optionType);
-    
+
     let promise = find.connectToApi(options);
-    
+
     promise.then(function(response) {
       let body = JSON.parse(response);
-      console.log(body);
+      // haven't refactored yet for doctor practices. still working on by name
       getDoctorInfo(body, option);
+      //$("#result").text(`Sorry, no doctors have the name ${option} in your area.`);
     }, function(error) {
-      $("#result").text(`Sorry, no doctors have the name ${option} in your area.`);
+      $("#errorMessage").text(`There was an error processing your request: ${error.message}`);
     });
   });
 
@@ -38,6 +40,7 @@ $(document).ready(function() {
 
     promise.then(function(response) {
       let body = JSON.parse(response);
+      getDoctorInfo(body, option);
       $('#result').text(`The doctors that practice ${option} are ${body.data.doctors[0].first_name}`);
     }, function(error) {
       // $("#result").text(`Sorry, no doctors practice ${option} in your area.`);
