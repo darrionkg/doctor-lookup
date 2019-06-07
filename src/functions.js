0753d2e332717bd542d63d25e49ede35179c170d
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-06-07 14:53:17 -0700

    add search options

--- src/functions.js
+++ src/functions.js
@@ -4,10 +4,13 @@ export class DoctorFinder {
 
   }
 
-  addToOptions(userOption) {
-    if(userOption != null) {
-      options = "";
-      options += userOption + "&";
+  addToOptions(userOption, optionType) {
+    let options = "";
+    if(userOption != null && optionType == "doctors") {
+      options += "doctors?name=" + userOption + "&";
+    }
+    else if(userOption != null && optionType == "practices") {
+      options += "practices?name=" + userOption + "&";
     }
     return options;
   }
@@ -16,7 +19,7 @@ export class DoctorFinder {
   const API_KEY = process.env.exports.apiKey;
   let promise = new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?${options}location=or-portland&user_location=45.5206223%2C-122.6773984&skip=0&limit=10&user_key=${API_KEY}`;
+    let url = `https://api.betterdoctor.com/2016-03-01/${options}location=or-portland&user_location=45.5206223%2C-122.6773984&skip=0&limit=10&user_key=${API_KEY}`;
     request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -3,12 +3,33 @@ import $ from 'jquery';
 // import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { DoctorFinder } from './functions';
 
 $(document).ready(function() {
+  let find = new DoctorFinder();
+  $("#nameForm").submit(function(event) {
+    event.preventDefault();
+    let option = $("#name").val();
+    let optionType = $("#optionType1").val();
+    let options = find.addToOptions(option, optionType);
+    find.connectToApi(options);
+
     promise.then(function(response) {
-      let body = JSON.parse(response);
-      console.log("Success");
+      $('#result').text(`The doctors with the name ${option} include ${body.data.doctors[0].first_name}%`);
     }, function(error) {
-      console.log("fail");
+      $("#result").text(`Sorry, no doctors have the name ${option} in your area.`);
     });
+  });
+
+  $("#practiceForm").submit(function(event) {
+    event.preventDefault();
+    let option = $("#practice").val();
+    let optionType = $("#optionType2").val();
+
+    promise.then(function(response) {
+      $('#result').text(`The doctors that practice ${option} are ${body.data.doctors[0].first_name}`);
+    }, function(error) {
+      $("#result").text(`Sorry, no doctors practice ${option} in your area.`);
+    });
+  });
 });
