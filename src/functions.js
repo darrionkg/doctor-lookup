40161797b0c9816f7a5a86abb3464b211665ab45
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-06-07 14:02:34 -0700

    Seperate logic and add addToOptions()

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,31 @@
-export function add(a, b) {
-    return a + b;
+export class DoctorFinder {
+
+  constructor() {
+
+  }
+
+  addToOptions(userOption) {
+    if(userOption != null) {
+      options = "";
+      options += userOption + "&";
+    }
+    return options;
+  }
+
+  connectToApi(options) {
+  const API_KEY = process.env.exports.apiKey;
+  let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?${options}location=or-portland&user_location=45.5206223%2C-122.6773984&skip=0&limit=10&user_key=${API_KEY}`;
+    request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 }
--- src/main.js
+++ src/main.js
@@ -5,35 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
-    // $.ajax({
-    //     url: `https://api.betterdoctor.com/2016-03-01/doctors?name=bob&location=45.5155%2C%20-122.6793%2C%20100&user_location=45.5155%2C%20-122.6793&skip=0&limit=10&user_key=${process.env.apiKey}`,
-    //     type: 'GET',
-    //     data: {
-    //       format: 'json'
-    //     },
-    //     success: function(response) {
-    //       console.log("Success")
-    //     },
-    //     error: function() {
-    //       console.log("There was an error processing your request. Please try again.");
-    //     }
-      // });
-    const API_KEY = process.env.exports.apiKey;
-    console.log(API_KEY);
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=45.520%2C-122.677&skip=0&limit=10&user_key=${API_KEY}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-
     promise.then(function(response) {
       let body = JSON.parse(response);
       console.log("Success");
